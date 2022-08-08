44be868462c4c786b61c124dbb93cb89b10d0583
PriDhingra <princedhingra25@gmail.com>
2022-08-07 22:29:03 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import SetAvatar from './pages/setAvatar';
+import SetAvatar from './components/setAvatar';
 
 const App = () => {
   return (
