a416fbde821ba54d24eed2ed3f3d8e621d2d33e2
PriDhingra <princedhingra25@gmail.com>
2022-08-07 21:52:45 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import SetAvatar from './pages/setAvatar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/setAvatar" element={<SetAvatar />}></Route>
+        <Route path="/" element={<Chat />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
